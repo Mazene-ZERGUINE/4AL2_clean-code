753675138d60cad82a26fceb086cdfe32cbe9883
Mazene <mmazenezerguine@gmail.com>
2023-12-25 10:30:34 +0100

    initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,18 @@
+import express, {Request, Response} from 'express';
+import {Express} from 'express';
+
+
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const port: number = Number(process.env.SERVER_PORT) || 3000;
+const app: Express = express();
+
+app.get("/", (req: Request, res: Response) => {
+	res.json("ping");
+});
+
+app.listen(port, () => {
+	console.log(`🚀 Server is running on port ${port}`);
+});
