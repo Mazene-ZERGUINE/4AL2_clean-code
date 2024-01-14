a5cbe5223f875745c6992055791baa2ede964e32
LoicZHU <zhu.loic@gmail.com>
2024-01-14 14:28:09 +0100

    chore(eslint): update config

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,28 +1,23 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'angular'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:angular/recommended',
-    'prettier',
-    'prettier/@typescript-eslint',
-    'prettier/angular',
-  ],
-  rules: {
-    "semi": ["error", "always"],
-    "no-console": "warn",
-    "no-unused-vars": ["error", { "args": "none" }],
-    "@typescript-eslint/naming-convention": [
-      "error",
-      { "selector": "variable", "format": ["camelCase", "UPPER_CASE"] },
-      { "selector": "function", "format": ["camelCase"] },
-      { "selector": "interface", "format": ["PascalCase"] }
-    ],
-    "@typescript-eslint/no-unused-vars": ["error", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
-    'no-console': 'warn',
-    'angular/controller-as-vm': ['error', 'never'],
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-  },
+	root: true,
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier',
+	],
+	rules: {
+		"semi": ["error", "always"],
+		"no-console": "warn",
+		"no-unused-vars": ["error", { "args": "none" }],
+		"@typescript-eslint/naming-convention": [
+			"error",
+			{ "selector": "variable", "format": ["camelCase", "UPPER_CASE"] },
+			{ "selector": "function", "format": ["camelCase"] },
+			{ "selector": "interface", "format": ["PascalCase"] }
+		],
+		"@typescript-eslint/no-unused-vars": ["error", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+	},
 };
