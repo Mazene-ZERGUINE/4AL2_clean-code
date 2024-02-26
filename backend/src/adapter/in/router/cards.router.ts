ed6a4e9bb0fc682194bcfedca2a10c0ef14b915a
LoicZHU <zhu.loic@gmail.com>
2024-02-26 22:50:04 +0100

    add cards

---
+++ backend/src/adapter/in/router/cards.router.ts
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+import { CardController } from '../controller/Card.controller';
+
+class CardsRouter {
+	private readonly _router = Router();
+
+	constructor() {
+		this.initRoutes();
+	}
+
+	get router(): Router {
+		return this._router;
+	}
+
+	initRoutes(): void {
+		this.router
+			.get('/', CardController.getAll)
+			.post('/', CardController.create)
+			.patch('/:cardId/answer', CardController.answerCard)
+			.get('/quizz', CardController.getQuizz);
+	}
+}
+
+export default new CardsRouter();
