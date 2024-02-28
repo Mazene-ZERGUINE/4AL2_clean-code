c478b8e4effc208a30023220163c89e6de5bc398
Mazene <mmazenezerguine@gmail.com>
2024-02-28 20:43:46 +0100

    updating cards in memory

--- backend/src/infrastructure/in-memory/cards/InMemoryCardRepository.ts
+++ backend/src/infrastructure/in-memory/cards/InMemoryCardRepository.ts
@@ -2,6 +2,7 @@ import { Card } from '../../../domain/card/entities/Card';
 import { randomUUID, UUID } from 'crypto';
 import { CardRepository } from '../../../domain/card/CardRepository';
 import { CardId } from '../../../domain/card/entities/CardId';
+import { Category } from '../../../domain/card/entities/Category';
 
 export class InMemoryCardRepository implements CardRepository {
 	private readonly registry = new Map<UUID, Card>();
@@ -56,24 +57,53 @@ export class InMemoryCardRepository implements CardRepository {
 			'What is the capital of Spain?',
 			'Madrid',
 			'geography',
+			Category.FIRST,
 		);
 		const historyCard1 = new Card(
 			new CardId(randomUUID()),
 			'When did the first world war start?',
 			'1914',
 			'history',
+			Category.FIFTH,
 		);
 		const historyCard2 = new Card(
 			new CardId(randomUUID()),
 			'When did the second world war start?',
 			'1939',
 			'history',
+			Category.SECOND,
+		);
+		const historyCard3 = new Card(
+			new CardId(randomUUID()),
+			'Who is adolf hitler',
+			'the one who started world war 2',
+			'history',
+			Category.FORTH,
+		);
+
+		const programmingCard1 = new Card(
+			new CardId(randomUUID()),
+			'Who invented linux',
+			'trovald somthing',
+			'programming',
+			Category.SECOND,
+		);
+
+		const programmingCard2 = new Card(
+			new CardId(randomUUID()),
+			'Who is the best c programmer',
+			'of cours it is sannanes',
+			'programming',
+			Category.THIRD,
 		);
 
 		this.registry
 			.set(geographyCard1.cardId.value, geographyCard1)
 			.set(geographyCard2.cardId.value, geographyCard2)
 			.set(historyCard1.cardId.value, historyCard1)
-			.set(historyCard2.cardId.value, historyCard2);
+			.set(historyCard2.cardId.value, historyCard2)
+			.set(historyCard3.cardId.value, historyCard3)
+			.set(programmingCard1.cardId.value, programmingCard1)
+			.set(programmingCard2.cardId.value, programmingCard2);
 	}
 }
