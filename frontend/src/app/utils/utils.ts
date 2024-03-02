83b5aff06e101a4d5709016c4cfc2f5c7d103f08
Zakarya Lahmadi <zakarya.lahmadi@affluences.com>
2024-03-02 18:57:15 +0100

    adding tests for cardAdpaterUtils functions

--- frontend/src/app/utils/utils.ts
+++
@@ -1,8 +0,0 @@
-import { Card } from '../core/models/card.model';
-import { CardKeyType } from '../shared/variables/enum';
-
-export const getDistinctValuesFromCardArray = (cards: Card[], key: CardKeyType): string[] => {
-  const distintValuesArray = new Set(cards.map((card) => card[key].toLowerCase()));
-
-  return [...distintValuesArray];
-};
